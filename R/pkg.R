8532c2fa2fd208034d8945fd32b840f7f48bba28
Jakson Alves de Aquino <jalvesaq@gmail.com>
2016-11-13 08:39:00 -0300

    Add option DataEntry.autostart

--- R/pkg.R
+++ R/pkg.R
@@ -3,9 +3,18 @@ DEenv <- new.env()
 
 .onAttach <- function(...){
     if(interactive()){
-        packageStartupMessage(paste0("\n",
-                                     gettext("If necessary, type 'DataEntry()' to start the application again.",
-                                             domain = "R-DataEntry"), "\n"))
-        DataEntry()
+        startnow <- FALSE
+        if(is.null(getOption("DataEntry.autostart"))){
+            startnow <- TRUE
+        } else {
+            if(getOption("DataEntry.autostart") == TRUE)
+                startnow <- TRUE
+        }
+        if(startnow){
+            packageStartupMessage(paste0("\n",
+                                         gettext("If necessary, type 'DataEntry()' to start the application again.",
+                                                 domain = "R-DataEntry"), "\n"))
+            DataEntry()
+        }
     }
 }
